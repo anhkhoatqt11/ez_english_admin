b2fa9511a989fde06024ccd598540c5e1b27a676
Trương Anh Khoa <anhkhoatqt11@gmail.com>
2024-05-28 22:03:40 +0700

    fix: Fix display issue

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -234,7 +234,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
               <li>
                 <Link
                   href="/practice"
-                  className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${pathname.includes("practice") &&
+                  className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${pathname.includes("users") &&
                     "bg-graydark dark:bg-meta-4"
                     }`}
                 >
